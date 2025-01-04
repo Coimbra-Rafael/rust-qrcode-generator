d23db9767869c580542d6e70a3556587062d64c9
Coimbra-Rafael <rcoimbra2022@outlook.com>
2025-01-04 18:44:21 -0300

    feat:: melhorias visuais no QR code e colocando confirmação

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,31 @@
-use qrcode::QrCode;
 use qrcode::render::unicode;
+use qrcode::QrCode;
 use std::io;
 fn main() {
     println!("Enter the text to generate QR code:");
+
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let code = QrCode::new(input.trim()).unwrap();
-    let image = code.render::<unicode::Dense1x2>().build();
-    println!("{}", image);
+
+    let input = input.trim();
+
+    println!("Do you want to generate a QR code for the following input? (y/n)");
+
+    let mut confirmation = String::new();
+    io::stdin()
+        .read_line(&mut confirmation)
+        .expect("Failed to read confirmation line");
+
+    if confirmation.trim().to_lowercase() == "y" {
+        let code = QrCode::new(input.trim()).unwrap();
+        let image = code
+            .render::<unicode::Dense1x2>()
+            .dark_color(unicode::Dense1x2::Light)
+            .light_color(unicode::Dense1x2::Dark)
+            .build();
+
+        println!("{}", image);
+    } else {
+        println!("QR code generation canceled.");
+    }
 }
